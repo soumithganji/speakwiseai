f30fada10e060493f7e4bbe247d1c63a444ed1c5
soumithganji <soumithganji@gmail.com>
2023-04-09 16:31:38 +0530

    app update dialog

--- app/src/main/java/chat/gpt/speakwise/gpt3/ai/chatbot/Activities/MainActivity.java
+++ app/src/main/java/chat/gpt/speakwise/gpt3/ai/chatbot/Activities/MainActivity.java
@@ -1,20 +1,26 @@
 package chat.gpt.speakwise.gpt3.ai.chatbot.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
-
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.Task;
 import chat.gpt.speakwise.gpt3.ai.chatbot.R;
 import chat.gpt.speakwise.gpt3.ai.chatbot.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     String supportEmail = "speakwiseai@gmail.com";
+    String appPlayStoreLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             window.setStatusBarColor(getColor(R.color.primary_black));
         }
 
+        appPlayStoreLink = "https://play.google.com/store/apps/details?id=" + getPackageName();
+
         binding.rlNewChat.setOnClickListener(v -> initNewChat());
 
         binding.rlRateApp.setOnClickListener(v -> initRateApp());
@@ -35,6 +43,33 @@ public class MainActivity extends AppCompatActivity {
 
         binding.rlShareApp.setOnClickListener(v -> initShareApp());
 
+        //see if the particular release is mandatory or not from backend
+        checkAppUpdate();
+    }
+
+    private void checkAppUpdate() {
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(getApplicationContext());
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                showUpdateDialog();
+            }
+        });
+    }
+
+    private void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View customView = getLayoutInflater().inflate(R.layout.app_update_dialog, null);
+        builder.setView(customView);
+
+        customView.findViewById(R.id.cwUpdateNow).setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(appPlayStoreLink));
+            startActivity(browserIntent);
+        });
+
+//        builder.setCancelable(false);
+
+        builder.show();
     }
 
     private void initNewChat() {
@@ -42,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRateApp() {
-        Uri webUri = Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName());
+        Uri webUri = Uri.parse(appPlayStoreLink);
         Intent intent = new Intent(Intent.ACTION_VIEW, webUri);
         startActivity(intent);
     }
@@ -51,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         String message = "Check out SpeakWise - a free AI chatbot app powered by GPT-3!" +
                 " Perfect for interesting conversations on any topic." +
                 " Download it for free on the Google Play Store and share with your friends!\n" +
-                "https://play.google.com/store/apps/details?id=" + getPackageName();
+                appPlayStoreLink;
 
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
