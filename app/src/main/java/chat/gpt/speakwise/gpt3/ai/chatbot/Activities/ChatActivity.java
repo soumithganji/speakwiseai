142b8bd7696a2029583a59a543085384838ee9af
soumithganji <soumithganji@gmail.com>
2023-05-19 08:45:41 +0530

    startSpeechToText crash

--- app/src/main/java/chat/gpt/speakwise/gpt3/ai/chatbot/Activities/ChatActivity.java
+++ app/src/main/java/chat/gpt/speakwise/gpt3/ai/chatbot/Activities/ChatActivity.java
@@ -302,11 +302,15 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     private void startSpeechToText() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak something...");
-        startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        try {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak something...");
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Something went wrong...", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void loadInterstitialAd() {
